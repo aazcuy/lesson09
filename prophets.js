21698523de4f7c6288447d44c58e5788c277e8fe
aazcuy <ceciliaagustinaazcuy@gmail.com>
2022-03-03 19:15:03 -0500

    commit

--- prophets.js
+++ prophets.js
@@ -16,21 +16,25 @@ function displayProphets(prophet) {
     // Create elements to add to the document
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let p = document.createElement('p');
     let portrait = document.createElement('img');
+    
 
     // Change the textContent property of the h2 element to contain the prophet's full name
     h2.innerHTML = `${prophet.name} <strong>${prophet.lastname}</strong>`;
-
+    p.innerHTML = `Date of Birth: ${prophet.birthdate} 
+    <br>Place of Birth: ${prophet.birthplace}`;
+    
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
-    h2.setAttribute('alt', `Portait of  ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President
-    Date of Birth: ${prophet.birthdate} 
-    Place of Birth: ${prophet.birthplace}`);
+    portrait.setAttribute('alt', `Portait of  ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President`);
     portrait.setAttribute('loading', 'lazy');
 
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
+    card.appendChild(p);
     card.appendChild(portrait);
+    
 
     // Add/append the existing HTML div with the cards class with the section(card)
     cards.appendChild(card);
